8fdcd21ae181cfa40d0c7463e8d1a6963160b43b
KSAMK\hmile02 <mika.letonsaari@xamk.fi>
2018-05-13 14:09:13 +0300

    johdanto lisätty

---
+++ src/pages/johdanto/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h2>Johdanto tarinankerrontaan</h2>
+	
+	<h3>Johdanto tarinankerrontaan</h3>
+	
+	
+	
+	
+	<Link to="/">Pääsivu</Link>
+	
+  </div>
+)
+
+export default IndexPage
