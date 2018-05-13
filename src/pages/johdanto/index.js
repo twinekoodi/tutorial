1ee90a12e932c5e6a43cb97634b4b27274954bf0
KSAMK\hmile02 <mika.letonsaari@xamk.fi>
2018-05-13 17:43:47 +0300

    lisää tekstiä johdantoon

--- src/pages/johdanto/index.js
+++ src/pages/johdanto/index.js
@@ -13,6 +13,13 @@ const IndexPage = () => (
 
 	<p>Tarinoita luetaan usein lapsille, koska tarinat auttavat hahmottamaan kuinka maailma toimii ja miten siinä tulee toimia. Tarinat eivät kuitenkaan ole vain lapsille vaan me kaikki hahmotamme maailmaa niiden kautta.</p>
 	
+	<p>Joitakin esimerkkejä tarinoista, joita emme arkiajattelussa ehkä edes miellä tarinoiksi:
+	<ul>
+	<li>Monissa kouluissa vaaditaan motivaatiokirje kouluun hakiessa. Meillä kaikilla on käsitys siitä ketä olemme, mistä tulemme ja mitä haluamme elämässä tehdä. Koulumenestykseen ja motivaatioon vaikuttaa vahvasti se kuinka hyvin koulu sopii siihen tarinaan, jonka kautta ymmärrämme itseämme.
+	<li>Mielikuvamainonta kertoo tarinoita onnellisista ja menestyvistä ihmisistä. Tuotteen brändi muodostuu mielikuvista, joilla tuote on yhdistetty näihin tarinoihin. Houkuttelevat tarinat voivat muokata sitä mitä ajattelemme itsestämme ja siitä miten meidän tulisi toimia. 
+	<li>Myytit ovat tarinoita siitä kuinka varhaiset ihmiset ymmärsivät maailmankaikkeuden toimivan. Jotkut ajattelivat, että maailma on syntynyt munasta tai että sadosta pitää uhrata osa maailman kulkua ohjaaville jumalille. Nykyään tiede on antanut meille tarkan kuvan vaikkapa siitä miten aurinkokunta on syntynyt tai kuinka elämä on kehittynyt Maassa. Vaikka myytit ja tieteellinen maailmankuva ovat hyvin erilaisia, ymmärrämme molemmat tarinoina jotka selittävät meille maailman toimintaa.
+	</ul>
+	
 	
 	
 	https://fi.scoutwiki.org/Tarina
