71fcc528bf0467ccde25fa74befaeb387d38e091
KSAMK\hmile02 <mika.letonsaari@xamk.fi>
2018-05-13 19:23:22 +0300

    lsiää tekstiä

--- src/pages/johdanto/index.js
+++ src/pages/johdanto/index.js
@@ -11,18 +11,19 @@ const IndexPage = () => (
 	
 	<p>Tarinat, tai hienommin sanoen narratiivit, ovat ihmisten tapa järjestellä toisiinsa liittyviä tapahtumia merkityksellisiksi kokonaisuuksiksi. Ne voivat olla keksittyjä tai perustua todellisiin tapahtumiin.</p>
 
-	<p>Tarinoita luetaan usein lapsille, koska tarinat auttavat hahmottamaan kuinka maailma toimii ja miten siinä tulee toimia. Tarinat eivät kuitenkaan ole vain lapsille vaan me kaikki hahmotamme maailmaa niiden kautta.</p>
-	
-	<p>Joitakin esimerkkejä tarinoista, joita emme arkiajattelussa ehkä edes miellä tarinoiksi:
+	<p>Tarinoita luetaan usein lapsille, koska tarinat auttavat hahmottamaan kuinka maailma toimii ja miten siinä tulee toimia. Tarinat eivät kuitenkaan ole vain lapsille vaan me kaikki hahmotamme maailmaa niiden kautta. Tässä joitakin esimerkkejä tarinoista, joita emme arkiajattelussa ehkä edes miellä tarinoiksi:
 	<ul>
-	<li>Monissa kouluissa vaaditaan motivaatiokirje kouluun hakiessa. Meillä kaikilla on käsitys siitä ketä olemme, mistä tulemme ja mitä haluamme elämässä tehdä. Koulumenestykseen ja motivaatioon vaikuttaa vahvasti se kuinka hyvin koulu sopii siihen tarinaan, jonka kautta ymmärrämme itseämme.</li>
+	<li>Monissa kouluissa vaaditaan motivaatiokirje kouluun hakiessa. Meillä kaikilla on käsitys siitä ketä olemme, mistä tulemme ja mitä haluamme elämässä tehdä. Koulumenestykseen ja motivaatioon vaikuttaa vahvasti se kuinka hyvin kyseinen koulu sopii siihen tarinaan, jonka kautta ymmärrämme itseämme.</li>
 	<li>Mielikuvamainonta kertoo tarinoita onnellisista ja menestyvistä ihmisistä. Tuotteen brändi muodostuu mielikuvista, joilla tuote on yhdistetty näihin tarinoihin. Houkuttelevat tarinat voivat muokata sitä mitä ajattelemme itsestämme ja siitä miten meidän tulisi toimia. </li>
 	<li>Myytit ovat tarinoita siitä kuinka varhaiset ihmiset ymmärsivät maailmankaikkeuden toimivan. Jotkut ajattelivat, että maailma on syntynyt munasta tai että sadosta pitää uhrata osa maailman kulkua ohjaaville jumalille. Nykyään tiede on antanut meille tarkan kuvan vaikkapa siitä miten aurinkokunta on syntynyt tai kuinka elämä on kehittynyt Maassa. Vaikka myytit ja tieteellinen maailmankuva ovat hyvin erilaisia, ymmärrämme molemmat tarinoina jotka selittävät meille maailman toimintaa.</li>
 	</ul>
 	</p>
 	
+	<p>Tarinankerronta ja tarinoiden ymmärtäminen on ihmiselle hyvin luontaista toimintaa. Ihmisellä on oma muistityyppi, episodinen muisti, joka on erikoistunut muistamaan todellisia tai kuviteltuja koettuja tapahtumaketjuja. Tarinat ovat siis oleellinen osa sitä miten ihmiset ajattelevat ja ymmärtävät maailmaa.</p>
+	</p>
 	
-	https://fi.scoutwiki.org/Tarina
+	<p>Lue lisää: <br>
+	<a href="https://fi.scoutwiki.org/Tarina">Tarinoiden käyttö partiossa</a></p>
 	
 	<h3>Millaisia tarinoita on olemassa?</h3>
 	
