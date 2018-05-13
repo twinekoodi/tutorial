66a1c0ec4b003d9be8d6dbf7accd4e5a4b65e6f9
KSAMK\hmile02 <mika.letonsaari@xamk.fi>
2018-05-13 17:08:13 +0300

    versio2

--- src/pages/johdanto/index.js
+++ src/pages/johdanto/index.js
@@ -5,9 +5,19 @@ const IndexPage = () => (
   <div>
     <h2>Johdanto tarinankerrontaan</h2>
 	
-	<h3>Johdanto tarinankerrontaan</h3>
+	<h3>Miksi tarinoita?</h3>
+	
+	<p>Sana "tarina" saattaa tuoda ensimmäisenä mieleen lapsille kerrottavat sadut. Myös kaunokirjallisuudessa, elokuvissa ja television draamasarjoissa kerrotaan tarinoita. Mistä tarinoissa on kyse ja ovatko ne vain viihdettä?</p>
+	
+	<p>Tarinat, tai hienommin sanoen narratiivit, ovat ihmisten tapa järjestellä toisiinsa liittyviä tapahtumia merkityksellisiksi kokonaisuuksiksi. Ne voivat olla keksittyjä tai perustua todellisiin tapahtumiin.</p>
+
+	<p>Tarinoita luetaan usein lapsille, koska tarinat auttavat hahmottamaan kuinka maailma toimii ja miten siinä tulee toimia. Tarinat eivät kuitenkaan ole vain lapsille vaan me kaikki hahmotamme maailmaa niiden kautta.</p>
 	
 	
+	
+	https://fi.scoutwiki.org/Tarina
+	
+	<h3>Millaisia tarinoita on olemassa?</h3>
 	
 	
 	<Link to="/">Pääsivu</Link>
